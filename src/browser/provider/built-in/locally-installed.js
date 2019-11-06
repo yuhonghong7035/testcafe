d949cfb9f95c328099a33e2d7d3901b69f62d3a8
Alex Kamaev <alexikam@gmail.com>
2019-11-06 16:04:07 +0300

    23

--- src/browser/provider/built-in/locally-installed.js
+++ src/browser/provider/built-in/locally-installed.js
@@ -5,16 +5,22 @@ export default {
     isMultiBrowser: true,
 
     async openBrowser (browserId, pageUrl, browserName) {
+        console.log('locally installed open browser');
         const args  = browserName.split(' ');
         const alias = args.shift();
 
         const browserInfo    = await browserTools.getBrowserInfo(alias);
+
+        console.log('q');
+
         const openParameters = Object.assign({}, browserInfo);
 
         if (args.length)
             openParameters.cmd = args.join(' ') + (openParameters.cmd ? ' ' + openParameters.cmd : '');
 
         await browserTools.open(openParameters, pageUrl);
+
+        console.log('qq');
     },
 
     async isLocalBrowser () {
